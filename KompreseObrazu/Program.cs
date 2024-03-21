7b66f6b163ba8840c9135c7128e131c32ee1d28d
Matej Chmiel <matej.chmiel@student.vitkovickastredni.cz>
2024-03-21 13:55:59 +0100

    CV- pocet unikatnich barev

--- KompreseObrazu/Program.cs
+++ KompreseObrazu/Program.cs
@@ -1,19 +1,24 @@
-﻿namespace Komprese
+﻿﻿using System;
+using System.Collections.Generic;namespace Komprese
 {
     public class KompreseObrazu{
 
         public static void Main (String [] args){
         
-        
-
         //Cesta k testovacímu souboru
         String testFilePath = @"C:\Users\MatějChmiel\github-classroom\Vitkovicka-stredni-prumyslova-skola\cv-komprese-obrazu-v2-matejchmiel\KompreseObrazu\CSV\obr1-10.csv";
         
         //vytvoření instance třídy Obrazek
         Obrazek inputCSV = new Obrazek(testFilePath);
+        List<int> unikatniBarvy = inputCSV.PaletaBarevObrazku();
+        Dictionary<int, int> colorCounts = new Dictionary<int, int>();
+        
 
-        
-        
+        foreach (int color in unikatniBarvy)
+            {
+                int count = inputCSV.PocetUnikatnichBarev(color);
+                colorCounts.Add(color, count);
+            }
         //Test metody, která spočítá počet řádků vstupního obrázku
         Console.WriteLine("Počet vertikálních řádků {0}",inputCSV.CountLines(testFilePath));
         
@@ -21,6 +26,16 @@
         Console.WriteLine("Počet horizontální řádků {0}",inputCSV.CountSymbolInLine(testFilePath));
 
         inputCSV.vypisImg();
+        Console.WriteLine("-----------------------------------");
+        Console.WriteLine("Unikátní barvy:");
+        foreach(int element in unikatniBarvy){
+            Console.Write($"{element}, ");
+        }
+        Console.WriteLine("\n-----------------------------------");
+            foreach (var kvp in colorCounts)
+            {
+                Console.WriteLine($"Barva: {kvp.Key}, Počet výskytů: {kvp.Value}");
+            }
     }
 
     }
